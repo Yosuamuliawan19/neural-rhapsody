2f722184d268154b211d6d019a759b0bb29da857
yosuamuliawan19 <yosuamuliawan19@gmail.com>
2019-01-09 10:26:46 +0700

    added function to prepare input and lables

--- trainer/train.py
+++ trainer/train.py
@@ -1,7 +1,14 @@
 from music21 import converter, instrument, note, chord
 from google.cloud import storage
 import os
-
+import glob
+import numpy
+from keras.utils import np_utils
+#----Global Variables----
+current_epoch = 1
+GCSPath = "gs://music-lstm"
+input_length = 100
+n_vocab = 0
 #----Function to fetch midi files from Google Cloud Storage----
 def download_files_from_gcs():
     print("Initializing client")
@@ -23,7 +30,52 @@ def download_files_from_gcs():
         if(not blob.name.endswith("/")):
             blob.download_to_filename(blob.name)
     print("Finsihed loading songs from gcs")
+#----Function to read and parse notes from midi files
+def prepare_data():
+    global n_vocab
+    logger = open("testfile.txt", "w")
 
+    notes = []
+    print("Preparing data")
+    for file in glob.glob("midi_songs/*.mid"):
+        midi = converter.parse(file)
+        print("Reading %s" % file)
+        notes_to_parse = None
+        #To ensure notes and chords are from 1 instrument only
+        try:
+            notes_to_parse = instrument.partitionByInstrument(midi).parts[0].recurse() 
+        except: 
+            notes_to_parse = midi.flat.notes
+
+        for element in notes_to_parse:
+            if isinstance(element, note.Note):
+                notes.append(str(element.pitch))
+            elif isinstance(element, chord.Chord):
+                notes.append('.'.join(str(n) for n in element.normalOrder))
+    print(notes)
+    logger.write("This is notes")
+    logger.write(str(notes))
+    n_vocab = len(set(notes))
+    pitchnames = sorted(set(item for item in notes))
+     # create a dictionary to map pitches to integers
+    note_to_int = dict((note, number) for number, note in enumerate(pitchnames))
+    network_input = []
+    network_output = []
+    # create input sequences and the corresponding outputs
+    for i in range(0, len(notes) - input_length, 1):
+        sequence_in = notes[i:i + input_length]
+        sequence_out = notes[i + input_length]
+        network_input.append([note_to_int[char] for char in sequence_in])
+        network_output.append(note_to_int[sequence_out])
+    n_patterns = len(network_input)
+    # reshape the input into a format compatible with LSTM layers
+    network_input = numpy.reshape(network_input, (n_patterns, input_length, 1))
+    # normalize input
+    network_input = network_input / float(n_vocab)
+    network_output = np_utils.to_categorical(network_output)
+    print("VOCAB")
+    print(n_vocab)
+    return (network_input, network_output)
 if __name__ == "__main__":
 
     download_files_from_gcs()
